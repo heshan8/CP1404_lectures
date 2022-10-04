8ea91f23dd6e0da06492a2511b8600f241041343
Heshan Liyanage <heshan988@gmail.com>
2022-10-04 22:02:14 +1000

    Added lecture demos

--- week_03/week_03.py
+++ week_03/week_03.py
@@ -1,8 +1,20 @@
-def main():
-    infile = open("DEMO.md")
-    text = infile.readlines()
-    infile.close()
-    print(f"{text[0]}{text[1]}")
+# infile = open("demo.txt", "r")
+# lines = infile.readlines()
+# for line in lines:
+#     if line.startswith("#"):
+#         print(line.strip())
+#
 
 
-main()
+# name = input("file: ")
+# with open("test.txt", "w") as out_file:
+#     print(name, file=out_file)
+
+
+with open("test.txt", "r") as in_file:
+    for line in in_file:
+        parts = line.strip().split(",")
+        guitar = parts[0]
+        year = parts[1]
+        price = (parts[2]).strip("\\n")
+        print(f"{guitar} is made in {year} and costs ${price}")
