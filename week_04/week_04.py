7a566a07477b23f80064a9848288a73dbdd047f9
Heshan Liyanage <heshan988@gmail.com>
2022-10-05 21:18:55 +1000

    Added string formatting to aid error checking.

--- week_04/week_04.py
+++ week_04/week_04.py
@@ -4,9 +4,9 @@ while number != 0:
     try:
         print(names[number - 1])
     except IndexError:
-        print("Invalid number")
+        print(f"Number must be less than {len(names) + 1}")
     number = int(input(f"Enter number up to {len(names)}: "))
-
+print("Quit")
 
 
 
