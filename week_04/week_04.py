13a651b14d6abc0145e3c4ba2be5da2028215790
Heshan Liyanage <heshan988@gmail.com>
2022-10-06 21:20:18 +1000

    Completed do this now #3

--- week_04/week_04.py
+++ week_04/week_04.py
@@ -8,12 +8,17 @@
 #     number = int(input(f"Enter number up to {len(names)}: "))
 # print("Quit")
 
-from operator import itemgetter
+# from operator import itemgetter
+#
+# score_pairs = [['Derek', 7], ['Carrie', 8], ['Bob', 6]]
+#
+# new_score_pair = input("Enter name and score: ").split()
+# score_pairs.append(new_score_pair)
+# score_pairs[3][1] = int(new_score_pair[1])  # converting new_score_pair score to an int
+# score_pairs.sort(key=itemgetter(1), reverse=True)  # sorting by second index and reversing the order
+# print(score_pairs)
 
-score_pairs = [['Derek', 7], ['Carrie', 8], ['Bob', 6]]
+text = "This is a sentence".split()
+long_words = [word for word in text if len(word) > 3]
+print(long_words)
 
-new_score_pair = input("Enter name and score: ").split()
-score_pairs.append(new_score_pair)
-score_pairs[3][1] = int(new_score_pair[1])  # converting new_score_pair score to an int
-score_pairs.sort(key=itemgetter(1), reverse=True)  # sorting by second index and reversing the order
-print(score_pairs)
